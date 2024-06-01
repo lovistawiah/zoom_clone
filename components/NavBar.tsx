767ce5bf572fa0516d1456090e77c49a835d1281
lovistawiah <lovistawiah638@gmail.com>
2024-06-01 13:59:03 +0000

    FImplement responsive nav bar

---
+++ components/NavBar.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import MobileNav from "./MobileNav";
+
+const NavBar = () => {
+    return (
+        <nav className="flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-6 lg:px-10">
+            <Link href="/" className="flex items-center gap-e1">
+                <Image
+                    src="/icons/logo.svg"
+                    alt="Yoom logo"
+                    width={32}
+                    height={32}
+                />
+                <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+                    Yoom
+                </p>
+            </Link>
+            <div className="flex-between gap-5">
+                {/* Clerk User management */}
+                <MobileNav />
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
